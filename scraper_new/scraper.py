f616b377245eb7a97623dd7345ed3289cf1d110d
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2023-09-22 17:54:15 +0200

    aggiunta scraping per parole

--- scraper_new/scraper.py
+++ scraper_new/scraper.py
@@ -1,23 +1,28 @@
 import asyncio
 from twscrape import API, gather
 import pandas as pd
-import datetime
+from datetime import datetime
+import os
 
 
-async def get_user_tweet(api, user_id, limit): #https://tweeterid.com/
+async def get_word_tweet(api, word, limit):
+    all_tweet = await gather(api.search(word, limit=limit)) 
+    print("Tweet trovati con ",word, ":", len(all_tweet))
+    return all_tweet
+    
+    
+async def get_user_tweet(api, user, user_id, limit): #https://tweeterid.com/
     await api.user_by_id(user_id)  # User
 
     all_tweet = await gather(api.user_tweets(user_id, limit=limit))
+    print("Tweet trovati con ",user,':', len(all_tweet))
     return all_tweet
 
 def show_tweets(all_tweet):
-    num_tweet = 0
     for x in all_tweet: 
         print("------------------TWEET------------------")
         print( x.user.username, x.date, x.rawContent)
-        num_tweet = num_tweet + 1
-    
-    print("numero tweet: ", num_tweet)
+       
     
 def convert_to_csv(all_tweet, theme):
     db = pd.DataFrame(columns=['username',
@@ -32,9 +37,10 @@ def convert_to_csv(all_tweet, theme):
         
     current_datetime = datetime.now()
     # Format the current datetime as a string with a timestamp
-    today = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    today = current_datetime.strftime("%Y-%m-%d_%H:%M:%S")
     
     filename = f'{theme}_{today}.csv'
+    print('filename',filename)
     db.to_csv(f'./files/{filename}')
     
 
@@ -47,17 +53,24 @@ async def main():
     #Avvio le ricerche
     
     #1) @marwilliamson
-    result = await get_user_tweet(api, 21522338, 1)
-    print(result)
-    show_tweets(result)
-    convert_to_csv(result, '@marwilliamson')
+    user = '@marwilliamson'
+    result = await get_user_tweet(api,user, 21522338, 1)
+    #show_tweets(result)
+    convert_to_csv(result, user)
     
     #2) @Mike_Pence
-    result = await get_user_tweet(api, 22203756, 1)
+    user = '@Mike_Pence'
+    result = await get_user_tweet(api, user, 22203756, 1)
+    #show_tweets(result)
+    convert_to_csv(result, user)
+    
+    #3) #Democrats
+    word = '#Democrats'
+    result = await get_word_tweet(api, word, 1)
     show_tweets(result)
-    convert_to_csv(result, '@Mike_Pence')
+    convert_to_csv(result, word)
     
-    
+
     print('---------------------------------------------------------\nfine!')
    
  
