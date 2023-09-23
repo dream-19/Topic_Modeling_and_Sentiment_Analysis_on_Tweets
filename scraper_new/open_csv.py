42b09e9c8dd3878bd36499dc26b4a709972ff83d
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2023-09-23 11:15:58 +0200

    lista account

--- scraper_new/open_csv.py
+++ scraper_new/open_csv.py
@@ -2,12 +2,14 @@ import os
 import csv
 
 def read_csv(relative_path):
+    all_files = []
     tot_tweets = 0
     num_files = 0
     # Iterate through all files in the directory
     for filename in os.listdir(relative_path):
         # Construct the full file path
         file_path = os.path.join(relative_path, filename)
+        all_files.append(file_path)
         
         # Check if the file is a regular file (not a subdirectory)
         if os.path.isfile(file_path):
@@ -18,12 +20,16 @@ def read_csv(relative_path):
                 row_count = 0
                 with open(file_path, 'r') as csv_file:
                     csv_reader = csv.reader(csv_file)
+                    
                     for row in csv_reader:
                         row_count += 1
+                        
                     print("Dimensione file",filename, ":  ", row_count)
                     tot_tweets += row_count
                     num_files += 1
+                    
     print("\nTotale tweet: ", tot_tweets, "per totale file: ", num_files)
+    return all_files
 
 if __name__ == '__main__':
     read_csv('./files')
